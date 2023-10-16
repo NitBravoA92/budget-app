720522932eb1518afefebeaa2d8b8d42cdc99d91
Carmen Bravo <nbravoalcala@gmail.com>
2023-10-16 17:51:33 -0400

    Create a test to validate the amount attribute of Payment is greater than or equal to 0

--- spec/models/payment_spec.rb
+++ spec/models/payment_spec.rb
@@ -33,5 +33,10 @@ RSpec.describe Payment, type: :model do
 
       expect(payment).to_not be_valid
     end
+
+    it 'a payment transaction should not be valid if the amount is less than 0' do
+      payment.amount = -3.90
+      expect(payment).to_not be_valid
+    end
   end
 end
