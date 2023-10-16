a1d4aee0bc3b05cce95ab99333408627633aff10
Carmen Bravo <nbravoalcala@gmail.com>
2023-10-16 17:27:07 -0400

    Create the test to validate an instance of the Payment model

--- spec/models/payment_spec.rb
+++ spec/models/payment_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
-# RSpec.describe Payment, type: :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+RSpec.describe Payment, type: :model do
+  let(:user) { create(:user) }
+  let(:payment) { create(:payment, author: user) }
+
+  describe 'payment' do
+    it 'should be a valid instance of Payment class' do
+      expect(payment).to be_a(Payment)
+    end
+  end
+end
