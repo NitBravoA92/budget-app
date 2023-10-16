7bda56d462d4f49041e064816ca30bfc02dac28c
Carmen Bravo <nbravoalcala@gmail.com>
2023-10-16 16:31:34 -0400

    Create the test to validate an instance of the User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create(:user) }
+
+  describe 'user' do
+    it 'should be a valid instance of User class' do
+      expect(user).to be_a(User)
+    end
+  end
 end
