0e7f5525a9cdfc6fe934149e55b7d77522c76cce
Carmen Bravo <nbravoalcala@gmail.com>
2023-10-16 15:13:29 -0400

    Add the root route of the app

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get '/welcome', to: 'welcome_pages#index'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  root 'welcome_pages#index'
 
   # Defines the root path route ("/")
   # root "articles#index"
