f62b7d203ef5e37413fb96fc1eb1d0e456f2737f
Carmen Bravo <nbravoalcala@gmail.com>
2023-10-16 16:51:26 -0400

    Create the test to validate an instance of the Category model

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
-# RSpec.describe Category, type: :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+RSpec.describe Category, type: :model do
+  let(:category) { create(:category) }
+
+  describe 'category' do
+    it 'should be a valid instance of Category class' do
+      expect(category).to be_a(Category)
+    end
+  end
+end
