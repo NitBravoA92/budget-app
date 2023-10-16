faa586c7034abc2de12da6a638744e9777f53f1e
Carmen Bravo <nbravoalcala@gmail.com>
2023-10-16 19:49:54 -0400

    Remove unnecessary tests

--- spec/requests/welcome_pages_spec.rb
+++ spec/requests/welcome_pages_spec.rb
@@ -1,10 +1,1 @@
 require 'rails_helper'
-
-# RSpec.describe 'WelcomePages', type: :request do
-#   describe 'GET /index' do
-#     it 'returns http success' do
-#       get '/welcome_pages/index'
-#       expect(response).to have_http_status(:success)
-#     end
-#   end
-# end
