611eeb7d7a9d64ee9cb29ddb7db04e1275e86209
Carmen Bravo <nbravoalcala@gmail.com>
2023-10-16 14:28:18 -0400

    Add the has_and_belongs_to_many association with the Payment model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,6 @@
 class Category < ApplicationRecord
   belongs_to :user
+  has_and_belongs_to_many :payments
 
   validates :name, :icon, presence: true, 
   validates :name, length: { maximum: 255 }
