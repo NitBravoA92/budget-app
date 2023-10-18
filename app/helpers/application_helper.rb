5f30ab788405300798002e44f10582032a9020e8
Carmen Bravo <nbravoalcala@gmail.com>
2023-10-18 14:15:30 -0400

    Create the 'generate_general_links' method in ApplicationHelper module

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,27 @@
 module ApplicationHelper
+  def generate_general_links
+    btn_back = 'B'
+    authenticated_data = {
+      'categories' => {
+        'index' => {
+          'back' => "#{link_to btn_back, categories_path }",
+          'title' => 'CATEGORIES'
+        },
+        'new' => {
+          'back' => "#{link_to btn_back, categories_path }",
+          'title' => 'NEW CATEGORY'
+        }
+      },
+      'payments' => {
+        'index' => {
+          'back' => "#{link_to btn_back, categories_path }",
+          'title' => 'TRANSACTIONS'
+        },
+        'new' => {
+          'back' => "#{link_to btn_back, categories_path }",
+          'title' => 'NEW TRANSACTION'
+        }
+      },
+    }
+  end
 end
