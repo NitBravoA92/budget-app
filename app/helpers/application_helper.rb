e8cffca032b1e7c7c9c39af3a358ac39769fa49d
Carmen Bravo <nbravoalcala@gmail.com>
2023-10-18 14:16:22 -0400

    Create the 'generate_devise_links' method in the ApplicationHelper module

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,38 @@
 module ApplicationHelper
+  def generate_devise_links
+    btn_back = 'B'
+    session_data = {
+      'sessions' => {
+        'new' => {
+          'back' => "#{link_to btn_back, root_path }",
+          'title' => 'LOG IN'
+        }
+      },
+      'passwords' => {
+        'new' => {
+          'back' => "#{link_to btn_back, new_user_session_path }",
+          'title' => 'FORGOT YOUR PASSWORD'
+        },
+        'edit' => {
+          'back' => "#{link_to btn_back, new_user_session_path }",
+          'title' => 'CHANGE YOUR PASSWORD'
+        }
+      },
+      'confirmations' => {
+        'new' => {
+          'back' => "#{link_to btn_back, new_user_session_path }",
+          'title' => 'RESEND CONFIRMATION'
+        }
+      },
+      'registrations' => {
+        'new' => {
+          'back' => "#{link_to btn_back, root_path }",
+          'title' => 'SIGN UP'
+        }
+      },
+    }
+  end
+
   def generate_general_links
     btn_back = 'B'
     authenticated_data = {
