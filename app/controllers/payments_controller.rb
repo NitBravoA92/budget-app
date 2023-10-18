d1bc33d9f19ee5b1c0553b95742712426604b11b
Carmen Bravo <nbravoalcala@gmail.com>
2023-10-18 00:20:22 -0400

    Retrieve all the payments for a given category - index action of PaymentsController

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,4 +1,7 @@
 class PaymentsController < ApplicationController
   before_action :authenticate_user!, only: %i[index]
-  def index; end
+  def index
+    @payments = current_user.payments.includes(:categories).where(categories: { id:  params[:category_id] }).order(created_at: :desc)
+    @total_amount = @payments.sum(:amount)
+  end
 end
