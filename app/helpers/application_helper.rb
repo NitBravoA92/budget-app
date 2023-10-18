4b6874316f0cd3936c4cf4e5967daed463fcc8ed
Carmen Bravo <nbravoalcala@gmail.com>
2023-10-18 16:51:43 -0400

    Change the color of the 'arrow' icon in the navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,15 @@
 module ApplicationHelper
+  def generate_arrow
+    '<svg xmlns="http://www.w3.org/2000/svg" height="40" viewBox="0 -960 960 960" width="40"><path fill="white" d="m313-440 224 224-57 56-320-320 320-320 57 56-224 224h487v80H313Z"/></svg>'
+  end
+
   def generate_navigation
     content = ''
     return if controller_name == 'welcome_pages'
     sign_out = user_signed_in? ? "#{link_to 'Sign Out', destroy_user_session_path, data: { turbo_method: :delete } }" : ''
 
     navbar = "<nav class='w-full bg-main-700'>
-      <div class='container mx-auto py-6 px-4'>
+      <div class='container mx-auto py-4 px-4'>
         <ul class='w-full flex justify-between items-center gap-2 text-lg'>
           <li class='w-1/6 text-white'>
             #{generate_navbar_data['back'] unless generate_navbar_data.nil?}
@@ -22,66 +26,65 @@ module ApplicationHelper
   end
 
   def generate_navbar_data
-    sessions = generate_devise_links
-    generals = generate_general_links
+    icon = "#{generate_arrow}".html_safe
+    sessions = generate_devise_links(icon)
+    generals = generate_general_links(icon)
     header = sessions.merge(generals)
     header[controller_name][action_name]
   end
 
-  def generate_devise_links
-    btn_back = 'B'
+  def generate_devise_links(icon)
     session_data = {
       'sessions' => {
         'new' => {
-          'back' => "#{link_to btn_back, root_path }",
+          'back' => "#{link_to icon, root_path }",
           'title' => 'LOG IN'
         }
       },
       'passwords' => {
         'new' => {
-          'back' => "#{link_to btn_back, new_user_session_path }",
+          'back' => "#{link_to icon, new_user_session_path }",
           'title' => 'FORGOT YOUR PASSWORD'
         },
         'edit' => {
-          'back' => "#{link_to btn_back, new_user_session_path }",
+          'back' => "#{link_to icon, new_user_session_path }",
           'title' => 'CHANGE YOUR PASSWORD'
         }
       },
       'confirmations' => {
         'new' => {
-          'back' => "#{link_to btn_back, new_user_session_path }",
+          'back' => "#{link_to icon, new_user_session_path }",
           'title' => 'RESEND CONFIRMATION'
         }
       },
       'registrations' => {
         'new' => {
-          'back' => "#{link_to btn_back, root_path }",
+          'back' => "#{link_to icon, root_path }",
           'title' => 'SIGN UP'
         }
       },
     }
   end
 
-  def generate_general_links
-    btn_back = 'B'
+  def generate_general_links(icon)
     authenticated_data = {
       'categories' => {
         'index' => {
-          'back' => "#{link_to btn_back, categories_path }",
+          'back' => "#{link_to icon, categories_path }",
           'title' => 'CATEGORIES'
         },
         'new' => {
-          'back' => "#{link_to btn_back, categories_path }",
+          'back' => "#{link_to icon, categories_path }",
           'title' => 'NEW CATEGORY'
         }
       },
       'payments' => {
         'index' => {
-          'back' => "#{link_to btn_back, categories_path }",
+          'back' => "#{link_to icon, categories_path }",
           'title' => 'TRANSACTIONS'
         },
         'new' => {
-          'back' => "#{link_to btn_back, categories_path }",
+          'back' => "#{link_to icon, :back }",
           'title' => 'NEW TRANSACTION'
         }
       },
