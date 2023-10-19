1a4e5bf48357680a5be523509f28fca0601e6dcb
Carmen Bravo <nbravoalcala@gmail.com>
2023-10-19 17:13:19 -0400

    Create the test to validate the 'GET /new' request - Payments

--- spec/requests/payments_spec.rb
+++ spec/requests/payments_spec.rb
@@ -32,4 +32,30 @@ RSpec.describe 'Payments', type: :request do
       expect(response.body).to include(expected_result_payment)
     end
   end
+
+  # request to: /payments/new
+  describe 'GET /new' do
+    before do
+      user = FactoryBot.create(:user)
+
+      sign_in user
+      get new_payment_path
+    end
+
+    # test if the response status was correct (status 200)
+    it 'returns http success' do
+      expect(response).to have_http_status(:success)
+    end
+
+    # test if a correct template was rendered.
+    it 'renders the new template' do
+      expect(response).to render_template(:new)
+    end
+
+    # test If the response body includes correct content.
+    it 'renders the new template with correct content' do
+      expected_result_title = 'ADD PAYMENT'
+      expect(response.body).to include(expected_result_title)
+    end
+  end
 end
