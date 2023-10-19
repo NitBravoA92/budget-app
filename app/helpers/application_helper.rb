a6f32669a05dba417f71dc1a4deeea1ea4311f11
Carmen Bravo <nbravoalcala@gmail.com>
2023-10-19 02:38:03 -0400

    Create a navbar for devise pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,33 +3,58 @@ module ApplicationHelper
     '<svg xmlns="http://www.w3.org/2000/svg" height="30" viewBox="0 -960 960 960" width="30"><path fill="white" d="m313-440 224 224-57 56-320-320 320-320 57 56-224 224h487v80H313Z"/></svg>'
   end
 
-  def generate_navigation
+  def generate_general_navigation
     content = ''
     return if controller_name == 'welcome_pages'
-    sign_out = user_signed_in? ? "#{link_to 'Sign Out', destroy_user_session_path, data: { turbo_method: :delete } }" : ''
+
+    data = generate_navbar_data(true)
+
+      navbar = "<nav class='w-full bg-main-700 fixed top-0 left-0 right-0 z-10'>
+        <div class='container mx-auto py-4 px-4'>
+          <ul class='w-full flex justify-between items-center gap-2'>
+            <li class='w-1/6 text-white'>
+              #{data['back'] unless data.nil?}
+            </li>
+            <li class='w-3/6 text-white text-semibold uppercase text-center text-xl'>
+              #{data['title'] unless data.nil?}
+            </li>
+            <li class='w-2/6 text-white text-right text-lg'>
+              #{link_to 'Sign Out', destroy_user_session_path, data: { turbo_method: :delete } }
+            </li>
+          </ul>
+        </div>
+      </nav>"
+      content << navbar
+    content.html_safe
+  end
+
+  def generate_auth_navigation(form)
+    content = ''
+    data = generate_navbar_data(false)
 
     navbar = "<nav class='w-full bg-main-700 fixed top-0 left-0 right-0 z-10'>
       <div class='container mx-auto py-4 px-4'>
         <ul class='w-full flex justify-between items-center gap-2'>
           <li class='w-1/6 text-white'>
-            #{generate_navbar_data['back'] unless generate_navbar_data.nil?}
+            #{data['back'] unless data.nil?}
           </li>
           <li class='w-3/6 text-white text-semibold uppercase text-center text-xl'>
-            #{generate_navbar_data['title'] unless generate_navbar_data.nil?}
+            #{data['title'] unless data.nil?}
           </li>
-          <li class='w-2/6 text-white text-right text-lg'>#{sign_out}</li>
+          <li class='w-2/6 text-white text-right text-lg'>
+            #{ form.submit data['right'] }
+          </li>
         </ul>
       </div>
     </nav>"
     content << navbar
+
     content.html_safe
   end
 
-  def generate_navbar_data
+  def generate_navbar_data(is_auth)
     icon = "#{generate_arrow}".html_safe
-    sessions = generate_devise_links(icon)
-    generals = generate_general_links(icon)
-    header = sessions.merge(generals)
+    header = is_auth ? generate_general_links(icon) : generate_devise_links(icon)
     header[controller_name][action_name]
   end
 
@@ -38,29 +63,34 @@ module ApplicationHelper
       'sessions' => {
         'new' => {
           'back' => "#{link_to icon, root_path }",
-          'title' => 'LOG IN'
+          'title' => 'LOG IN',
+          'right' => 'Log in'
         }
       },
       'passwords' => {
         'new' => {
           'back' => "#{link_to icon, new_user_session_path }",
-          'title' => 'FORGOT YOUR PASSWORD'
+          'title' => 'FORGOT YOUR PASSWORD',
+          'right' => 'Reset'
         },
         'edit' => {
           'back' => "#{link_to icon, new_user_session_path }",
-          'title' => 'CHANGE YOUR PASSWORD'
+          'title' => 'CHANGE YOUR PASSWORD',
+          'right' => 'Save'
         }
       },
       'confirmations' => {
         'new' => {
           'back' => "#{link_to icon, new_user_session_path }",
-          'title' => 'RESEND CONFIRMATION'
+          'title' => 'RESEND CONFIRMATION',
+          'right' => 'Resent'
         }
       },
       'registrations' => {
         'new' => {
           'back' => "#{link_to icon, root_path }",
-          'title' => 'SIGN UP'
+          'title' => 'SIGN UP',
+          'right' => 'Next'
         }
       },
     }
