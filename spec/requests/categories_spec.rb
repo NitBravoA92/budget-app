eea61511c423ec35c32620fed5cc6483b9fe41e2
Carmen Bravo <nbravoalcala@gmail.com>
2023-10-19 16:22:46 -0400

    Create the test to validate the 'GET /index' request - Categories

--- spec/requests/categories_spec.rb
+++ spec/requests/categories_spec.rb
@@ -1,1 +1,32 @@
 require 'rails_helper'
+
+RSpec.describe 'Categories', type: :request do
+  # request to: /categories
+  describe 'GET /index' do
+    before do
+      user = FactoryBot.create(:user)
+      FactoryBot.create(:category, user:)
+      sign_in user
+      get categories_path
+    end
+
+    # test if the response status was correct (status 200)
+    it 'returns http success' do
+      expect(response).to have_http_status(:success)
+    end
+
+    # test if a correct template was rendered.
+    it 'renders the index template' do
+      expect(response).to render_template(:index)
+    end
+
+    # test If the response body includes correct content.
+    it 'renders the index template with correct content' do
+      expected_result = 'CATEGORIES'
+      expect(response.body).to include(expected_result)
+
+      expected_result_categ = 'Category 1'
+      expect(response.body).to include(expected_result_categ)
+    end
+  end
+end
