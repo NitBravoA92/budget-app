9319ccdcd47771032af5bdd2d2dbd24bf8417520
Carmen Bravo <nbravoalcala@gmail.com>
2023-10-19 15:07:28 -0400

    Update the factory data of the category to add support for uploading files

--- spec/factories/categories.rb
+++ spec/factories/categories.rb
@@ -2,6 +2,11 @@ FactoryBot.define do
   factory :category do
     user
     name { 'Category 1' }
-    icon { 'https://www.imageicon.com/test' }
+    icon do
+      io = File.open(Rails.root.join('spec', 'icons', 'grocery_icon.jpg'))
+      filename = 'grocery_icon.jpg'
+      content_type = 'image/jpeg'
+      Rack::Test::UploadedFile.new(io, content_type, filename)
+    end
   end
 end
